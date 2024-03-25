549da576ee499b1f117c586974467649419b6c42
reboot <moniroka_339@hotmail.com>
2024-03-25 12:18:55 +0000

    adding shift system

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ function Enemy (health, stamina, strength) {
     this.attackEnemy = function() {
         return this.strength;
     },
-    this.specialAttackPlayer = function() {
+    this.specialAttackEnemy = function() {
         if (this.stamina >= 10) {
             this.stamina -= 10;
             return this.strength + 10 * Math.round(Math.random() * 3);
@@ -40,6 +40,12 @@ function Enemy (health, stamina, strength) {
     },
     this.receiveDamage = function (damage) {
         this.health -= damage;
+    },
+    this.healing = function() {
+        if (this.stamina >= 20) {
+            this.stamina -= 20;
+            this.health += 50;
+        }
     }
 }
 
@@ -50,14 +56,46 @@ function Game() {
 let player = new Player(100, 100, 20);
 let enemy = new Enemy(100, 100, 18);
 
+let actionsArray = ['attack', 'special', 'heal'],
+    randomAction = Math.floor(Math.random() * 3);
+    
+
+function actionsEnemy(action) {
+    switch(action) {
+        case 0:
+            let enemyStrength = enemy.attackEnemy();
+            player.receiveDamage(enemyStrength);
+            break;
+        case 1:
+            let specialEnemyStrength = enemy.specialAttackEnemy();
+            player.receiveDamage(specialEnemyStrength);
+            break;
+        case 2:
+            enemy.healing();
+            break;
+    }
+}
+
+
 $$('#basic-attack').addEventListener('click', function() {
     let playerStrength = player.attackPlayer();
     enemy.receiveDamage(playerStrength);
+    setTimeout(actionsEnemy, 1000, randomAction);
+    setTimeout(console.log, 2000, player.health);
+    console.log(player.health);
+    console.log(`enemy's stamina`, enemy.stamina);
+    console.log(`enemy's health`, enemy.health);
 })
 $$('#special-attack').addEventListener('click', function() {
     let playerStrength = player.specialAttackPlayer();
     enemy.receiveDamage(playerStrength);
+    //actionsEnemy(randomAction);
+    setTimeout(actionsEnemy(randomAction), 8000);
 })
 $$('#heal-action').addEventListener('click', function() {
     player.healing();
+    //actionsEnemy(randomAction);
+    setTimeout(actionsEnemy(randomAction), 8000);
 })
+
+
