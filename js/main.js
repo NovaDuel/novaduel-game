e319c010c1393f8542e5095b1c442526377edd82
reboot <moniroka_339@hotmail.com>
2024-03-26 11:38:59 +0000

    refactoring...in modules

--- js/main.js
+++ js/main.js
@@ -1,33 +1,10 @@
+import { Player } from "./modules/player.js";
+
 const selector = selector => document.querySelector(selector);
 const selectAll = selector => document.querySelectorAll(selector);
 let basicAttackBtn = selector('#basic-attack')
 let specialAttackBtn = selector('#special-attack');
 let healBtn = selector('#heal-action');
-
-function Player (health, stamina, strength) {
-    this.health = health;
-    this.stamina = stamina;
-    this.strength = strength;
-    this.attackPlayer = function() {
-        return this.strength;
-    },
-    this.specialAttackPlayer = function() {
-        if (this.stamina >= 10) {
-            this.stamina -= 10;
-            return this.strength + 10 * Math.round(Math.random() * 3);
-        }
-    },
-    this.receiveDamage = function(damage) {
-        this.health -= damage;
-    }
-    this.healing = function() {
-        if (this.stamina >= 20) {
-            this.stamina -= 20;
-            this.health += 30;
-        }
-    }
-}
-
 
 function Enemy (health, stamina, strength) {
     this.health = health;
